88e34bfe5dfb6e7a045a2ee78b47553582bd406e
rosemutai <chepngetichrose2030@gmail.com>
2023-03-31 11:59:25 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import { addToDoItem, displayAllTodos } from './modules/Todo.js';
 
+const button =  document.getElementById('clearBtn');
+
 const form = document.getElementById('add-form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
